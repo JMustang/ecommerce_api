b1cef115a37de15fb9cce3743dee7fa7a507e9a9
Junior <eecfredes@gmail.com>
2025-04-15 20:38:30 -0300

    :rocket:Category model, done

--- apiApp/models.py
+++ apiApp/models.py
@@ -12,9 +12,24 @@ class CustomUser(AbstractUser):
         return self.email
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to="category_img", black=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField()
     image = models.ImageField(upload_to="product_img", black=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
