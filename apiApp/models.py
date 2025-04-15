fd4ebde58270b97a0698873c465bb310dd2080fb
Junior <eecfredes@gmail.com>
2025-04-15 20:25:42 -0300

    :rocket:Product model, done

--- apiApp/models.py
+++ apiApp/models.py
@@ -10,3 +10,11 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField()
+    image = models.ImageField(upload_to="product_img", black=True, null=True)
