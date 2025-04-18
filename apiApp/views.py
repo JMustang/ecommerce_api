f36882fe7d3ce9d1420de6f99f40e000c8617bea
Junior <eecfredes@gmail.com>
2025-04-18 20:43:24 -0300

    :rocket:product detail view, created

--- apiApp/views.py
+++ apiApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductListSerializer
+from .serializers import ProductDetailSerializer, ProductListSerializer
 
 
 @api_view(["GET"])
@@ -10,3 +10,13 @@ def product_list(request):
     products = Product.objects.filter(featured=True)
     serializer = ProductListSerializer(products, many=True)
     return Response(serializer.data)
+
+
+@api_view(["GET"])
+def product_detail(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+        serializer = ProductDetailSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response({"error": "❌ Product not found!"})
