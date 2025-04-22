91e8d7b0cee1d1a9c71e1d765b0829fd21d8c5b8
Junior <eecfredes@gmail.com>
2025-04-21 23:42:28 -0300

    :bug:Fixing bugs

--- apiApp/admin.py
+++ apiApp/admin.py
@@ -5,22 +5,22 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
-class CustomUserAdmin:
+class CustomUserAdmin(UserAdmin):
     list_display = ("username", "email", "first_name", "last_name")
 
 
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "price", "featured")
 
 
-admin.site.register(ProductAdmin, Product)
+admin.site.register(Product, ProductAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
 
 
-admin.site.register(CategoryAdmin, Category)
+admin.site.register(Category, CategoryAdmin)
