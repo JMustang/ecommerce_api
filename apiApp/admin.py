9c13926d55c8123e8f9f5dabe8f82273833de7df
Junior <eecfredes@gmail.com>
2025-04-21 23:25:11 -0300

    :rocket: Custom user admin

--- apiApp/admin.py
+++ apiApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+class CustomUserAdmin:
+    list_display = ("username", "email", "first_name", "last_name")
+
+
+admin.site.register(CustomUser, UserAdmin)
