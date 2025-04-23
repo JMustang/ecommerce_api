0bde9451e9f99ca9336e77180b1a6ed5f62bfa35
Junior <eecfredes@gmail.com>
2025-04-22 22:12:00 -0300

    :rocket: Unique slug model

--- apiApp/models.py
+++ apiApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -14,7 +15,7 @@ class CustomUser(AbstractUser):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to="category_img", blank=True, null=True)
 
     def __str__(self):
@@ -38,3 +39,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+            if Product.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+                self.slug = unique_slug
+        super().save(*args, **kwargs)
