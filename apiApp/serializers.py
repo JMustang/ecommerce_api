df7fcb0dd30a390c6d71e5116ae4b0e55d822494
Junior <eecfredes@gmail.com>
2025-04-24 15:31:00 -0300

    :rocket: Cart item serializer, done

--- apiApp/serializers.py
+++ apiApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product
+from .models import CartItem, Category, Product
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -27,3 +27,16 @@ class CategoryDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name", "image", "products"]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    sub_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity", "sub_total"]
+
+    def get_sub_total(self, cartitem):
+        total = cartitem.product.price * cartitem.quatity
+        return total
