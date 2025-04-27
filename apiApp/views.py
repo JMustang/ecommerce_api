96f5d545901977fba327c9c673605bf6ea9286ee
Junior <eecfredes@gmail.com>
2025-04-27 20:34:17 -0300

    :rocket:Update cart item quantity, done

--- apiApp/views.py
+++ apiApp/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Cart, CartItem, Category, Product
 from .serializers import (
+    CartItemSerializer,
     CartSerializer,
     CategoryDetailSerializer,
     CategoryListSerializer,
@@ -59,3 +60,21 @@ def add_to_cart(request):
 
     serializer = CartSerializer(cart)
     return Response(serializer.data)
+
+
+@api_view(["PUT"])
+def update_cartitem_quantity(request):
+    cartitem_id = request.data.get("item_id")
+    quantity = request.data.get("quantity")
+
+    try:
+        cartitem = CartItem.objects.get(id=cartitem_id)
+        cartitem.quantity = quantity
+        cartitem.save()
+
+        serializer = CartItemSerializer(cartitem)
+        return Response(
+            {"data": serializer.data, "message": "Cart item updated successfully!"}
+        )
+    except CartItem.DoesNotExist:
+        return Response({"error": "❌ Cart item not found!"})
