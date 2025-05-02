4fa056bd0f8505cdc5de436ad8ff281baccfa65e
Junior <eecfredes@gmail.com>
2025-05-01 22:03:39 -0300

    :rocket:Import signals on apps.py

--- apiApp/apps.py
+++ apiApp/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ApiappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apiApp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apiApp"
+
+    def ready(self):
+        import apiApp.signals
