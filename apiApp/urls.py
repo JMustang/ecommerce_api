7efcb8d43ad35d821b0cf2ab8beb319775c1cae0
Junior <eecfredes@gmail.com>
2025-05-08 22:38:10 -0300

    :rocket:Search product view, done

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path("delete_review/<int:pk>/", views.delete_review, name="delete_review"),
     path("add_to_wishlist/", views.add_to_wishlist, name="add_to_wishlist"),
     path("delete_cartitem/<int:pk>/", views.delete_cartitem, name="delete_cartitem"),
+    path("search", views.product_search, name="search"),
 ]
